38d06b52bbda1e158d3f5f43805a03b5b0e3ba39
unknown <shaimazerin46@gmail.com>
2025-01-06 23:48:57 +0600

    service route for id done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
@@ -39,6 +39,12 @@ async function run() {
     });
     app.get('/services', async (req,res)=>{
       const result = await serviceCollections.find().toArray();
+      res.send(result)
+    })
+    app.get('/services/:id', async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await serviceCollections.findOne(query);
       res.send(result)
     })
     // Send a ping to confirm a successful connection
